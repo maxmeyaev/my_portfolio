8fb226c38b91d54f0f062264cd27453f6925e5e3
maxmeyaev <maxmeyaev05@gmail.com>
2022-04-20 23:55:38 -0400

    adjusted the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className='flex justify-between h-20 w-full items-center absolute z-10 text-white shadow-xx'>
         <div className=''>
-          <Logo width={80} height={80}/>
+          <Logo width={90} height={90}/>
         </div>
         <ul className='flex text-right'>
             <li className=''>About</li>
