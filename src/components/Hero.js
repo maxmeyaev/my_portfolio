dea0b2416b1045ad66f63dcbd060f3d6ce01aa25
maxmeyaev <maxmeyaev05@gmail.com>
2022-05-02 13:24:48 -0400

    fixed dropdown mobile menu

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,24 +2,24 @@ import React from 'react'
 import Email from '../icons/icon1.svg'
 import GithubLogo from '../icons/icon2.svg'
 export default function Hero() {
-  return (
-        <div className='w-full h-screen relative'>
-            <img src='/images/bg2.png' alt="bg-img" className='w-full h-screen object-cover' />
-            <div className='flex absolute w-full h-full top-0 flex-col justify-center items-center'>
-                <h2>Hi, I'm</h2>
-                <h1>Max Meyaev</h1>
-                <div className='flex bottom-0 absolute'>
-                    <a 
-                        href="" className='icons'>
-                        <img src={Email} alt="Email"/>
-                    </a>
-                    <a 
-                        href="https://github.com/maxmeyaev" target="_blank" 
-                        className='icons'>
-                        <img src={GithubLogo} alt="Github"/>
-                    </a>
+    return (
+            <div className='w-full h-screen relative'>
+                <img src='/images/bg2.png' alt="bg-img" className='w-full h-screen object-cover' />
+                <div className='flex absolute w-full h-full top-0 flex-col justify-center items-center'>
+                    <h2 className='text-2xl md:text-3xl'>Hi, I'm</h2>
+                    <h1 className='text-3xl md:text-5xl'>Max Meyaev</h1>
+                    <div className='flex bottom-0 absolute'>
+                        <a 
+                            href="" className='icons'>
+                            <img src={Email} alt="Email"/>
+                        </a>
+                        <a 
+                            href="https://github.com/maxmeyaev" target="_blank" 
+                            className='icons'>
+                            <img src={GithubLogo} alt="Github"/>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-  )
+    )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { useState } from 'react'
 import { ReactComponent as Logo } from '../icons/Rectangle.svg'
 import { ReactComponent as Arrow } from '../icons/arrow.svg'
 
-export const Navbar = () => {
-  const [nav, setNav] = useState(false) //need to use use state for hamburger menu dropdown
+export const Navbar = (props) => {
+  const [nav, setNav] = useState(false)
+  //need to use use state for hamburger menu dropdown
   const handleClick = () => {
-    setNav(!nav) //sets navbar to true on click
+  setNav(!nav) //sets navbar to true on click
   }
-  return (
+  return ( 
       <div className='flex justify-between h-20 w-full items-center absolute z-10 text-white shadow-xx'>
         <Logo width={90} height={90}/>
         <ul className='hidden md:flex'>
@@ -17,11 +18,11 @@ export const Navbar = () => {
           <li>Contact</li>
           <li>Resume</li>
         </ul>
-        <div onClick={handleClick} className='md:hidden'>
-          <Arrow className='arrow' fill='white'/>
+        <div onClick={handleClick} className='md:hidden cursor-pointer'>
+          <Arrow className='arrow cursor-pointer' fill='white'/>
         </div>
         {/* Hamburger Dropdown */}
-        <div onClick={handleClick} className={nav ? 'absolute top-0 left-0 w-full flex flex-col justify-center items-center' : 'absolute left-[-100%]'}>
+        <div onClick={handleClick} className={nav ? 'absolute top-0 right-0 w-36 mr-3 h-screen flex flex-col justify-center items-center' : 'absolute left-[-100%]'}>
           <ul className='flex flex-col items-center justify-center'>
             <li className='text-2xl'>About</li>
             <li className='text-2xl'>Projects</li>
